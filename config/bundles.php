9e481dbe0fe1a8e9f4ed62a82a74573f7d14d8c5
Github MMI3 <github@mmi-troyes.fr>
2026-02-11 11:18:34 +0000

    feat: add home page

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
